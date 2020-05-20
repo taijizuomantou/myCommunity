8a52eabcb9f3e59b4fa9b5dca0202e1feaaaa1c6
xue <719222033@qq.com>
2020-05-20 09:11:32 +0800

    2.4生成验证码完成

---
+++ src/main/java/com/nowcoder/community/config/KaptchaConfig.java
@@ -0,0 +1,33 @@
+package com.nowcoder.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+
+    @Bean//将会被Spring 容器装配
+    //kaptcha的核心是接口producer
+    public Producer kaptchaProduces(){
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width","100");
+        properties.setProperty("kaptcha.image.height","40");
+        properties.setProperty("kaptcha.textproducer.font.size","32");
+        properties.setProperty("kaptcha.textproducer.font.color","0,0,0");//表示黑色
+        //验证码中能用的字符
+        properties.setProperty("kaptcha.textproducer.char.string","0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length","4");
+        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise");//采用哪种干扰，不加干扰也可以
+
+        DefaultKaptcha kaptcha =new DefaultKaptcha();
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
