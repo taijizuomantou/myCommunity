e4c66c5cd9bfa7298311abfe1ca613322d389c8d
xue <719222033@qq.com>
2020-05-20 16:38:33 +0800

    2.7添加头像

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,107 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path="/setting", method = RequestMethod.GET)
+    public String getSettingPage(){
+        return "/site/setting";
+    }
+
+    @RequestMapping(path="/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if(headerImage == null){
+            model.addAttribute("error","您还没有选择图片");
+            return "/site/setting";
+        }
+
+        String fileName = headerImage.getOriginalFilename();
+
+        String suffix = fileName.substring(fileName.lastIndexOf('.'));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error","文件的格式不正确");
+            return "/site/setting";
+        }
+
+        //生成随机的文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        //确定文件的存放路径
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            //存储文件
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败"+e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常！",e);
+        }
+
+        //更新当前用户的头像路径。注意该路径不是本地路径而是web访问路径
+        //http://localhost:8080/community/user/header/xxxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath +"/user/header/"+fileName;
+        userService.updateHeader(user.getId(),headerUrl);
+        return "redirect:/index";
+    }
+
+    @RequestMapping(path="/header/{fileName}",method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName")String fileName, HttpServletResponse response){
+        //服务器存放的路径
+        fileName = uploadPath + "/" + fileName;
+        //解析文件的后缀 向浏览器输出图片。文件的格式就是文件的后缀。
+        String suffix = fileName.substring(fileName.lastIndexOf('.'));
+        //响应图片
+        response.setContentType("image/" + suffix);
+        try (
+                OutputStream os = response.getOutputStream();
+                FileInputStream fis = new FileInputStream(fileName);//自动关闭
+        ){
+
+            //声明一个缓冲区
+            byte[]buffer = new byte[1024];
+            int b = 0;
+            while((b = fis.read(buffer))!=-1){
+                os.write(buffer,0,b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败"+e.getMessage());
+        }
+    }
+
+}
