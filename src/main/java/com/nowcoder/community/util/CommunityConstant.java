5d4c07da547ef8ce83a5d17ee08e558f982b37d2
xue <719222033@qq.com>
2020-05-22 08:43:28 +0800

    3.5 显示评论

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -26,4 +26,14 @@ public interface CommunityConstant {
     记住我的情况下的登陆凭证超时时间
      */
     int REMEBER_EXPIRED_SECONDS = 3600*24*100;
+
+    /*
+    实体类型：帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /*
+    实体类型：评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
 }
